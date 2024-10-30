33da047a46ea380681ccd823dfa2dab895b0d115
aviennn <avienflairebatul@gmail.com>
2024-10-30 14:02:26 +0800

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Module05Exercise01.Services;
+using MySql.Data.MySqlClient;
+
+namespace Module05Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private readonly DatabaseConnectionService _dbConnectionService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Initialize Database Connection
+            _dbConnectionService = new DatabaseConnectionService();
+        }
+
+        
+
+        private async void OpenViewEmployee(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ViewEmployee");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Module05Exercise01
+
+## Landing Page
+![1](mod05exe01pics/1.png)
+
+## Employee Page
+![2](mod05exe01pics/2.png)
+
+
+# Module05-Exercise02
